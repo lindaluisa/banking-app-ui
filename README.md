2246176a5283fe51ac0c9f87447f19901a73fb01
lindaluisa <lindaluisarosenthal@gmail.com>
2021-09-09 09:47:39 +0200

    adding QR codeJS API

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # banking-app-ui
+
+Adding one feature at a time using **HTML, CSS and Vanilla JS** for now.
+
+
+Code base:
+- [x] HTML, CSS, Vanilla JS
+- [x] User-friendly Design
+- [x] Mobile First
+- [x] QR Code API
+- [x] CSS: Flexbox, Toggle
+- [x] JS: API, input validation
+
+
+
+Next features:
+- [x] Login System with Node.js
+- [x] Moving to React
+- [x] Media Queries
+- [x] ...
+- [x] ...
+- [x] ...any ideas? : )
--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 
-
-
+// TOGGLE EXPENSES
 function toggleExpenses() {
   var element = document.getElementById("b-toggle-trans");
   element.classList.toggle("b-transactions-up");
 }
+
+
+// let qrform = document.getElementById('qr-form');
+
+// let qrdata = document.getElementById('qr-data');
+
+// const qrcode = new QRCode(document.getElementById("qrcode"), {
+//   width: 200,
+//   height: 200,
+//   colorDark : "#2D2038",
+//   colorLight : "#e14ea6",
+// });
+// // #ac278a
+
+// function generateQR() {
+//   let data = qrdata;
+//   qrcode.clear();
+//   qrcode.makeCode(data);
+// }
+
