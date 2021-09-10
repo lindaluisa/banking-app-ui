4aba3d7ef59e07d9e35c56b72d9324bb62610d55
lindaluisa <lindaluisarosenthal@gmail.com>
2021-09-10 12:14:47 +0200

    adding User schema and mongoose for error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,12 @@
 const User = require('../models/User');
 
 
+// ERROR HANDLING
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let error = {email:'', password:''};
+}
+
 module.exports.signup_get = (req,res) => {
   res.render('signup'); 
 }
@@ -18,7 +24,7 @@ module.exports.signup_post = async(req,res) => {
     res.status(201).json(user);
   }
   catch (err) {
-    console.log(err);
+    handleErrors(err);
     res.status(400).send('error, please try again');
   }
 }
--- models/User.js
+++ models/User.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator'); 
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter your email'],
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: [isEmail, 'Please insert a valid email']
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6
+    required: [true, 'You need to specify a password'],
+    minlength: [6, 'The password must be at least 6 characters long']
   }
 })
 
 // model based on schema
-// name (e.g., 'user') must be the singular of database collection name
+// name (e.g., 'user') must be the singular of db collection name
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
